a7c51c5c02912cc526207179d92eab2d66548f92
Viktor Benei <viktor.benei@gmail.com>
2016-09-10 12:19:43 +0200

    fix in step.sh
    for older Bitrise CLIs without Go Toolkit support

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@ set -e
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 go_package_name="github.com/bitrise-io/steps-slack-message"
-full_package_path="${THIS_SCRIPT_DIR}/go/${go_package_name}"
+full_package_path="${THIS_SCRIPT_DIR}/go/src/${go_package_name}"
 mkdir -p "${full_package_path}"
 
 rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
