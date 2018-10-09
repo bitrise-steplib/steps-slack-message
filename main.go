4be733d1cf077f68a197b504e0d1106b0645c5ff
Akos Birmacher <akos.birmacher@bitrise.io>
2018-10-09 17:32:57 +0200

    ref: revert the webhook remove

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ type Config struct {
 	Debug bool `env:"is_debug_mode,opt[yes,no]"`
 
 	// Message
-	APIToken         stepconf.Secret `env:"api_token,required"`
+	WebhookURL       stepconf.Secret `env:"webhook_url"`
+	APIToken         stepconf.Secret `env:"api_token"`
 	Channel          string          `env:"channel"`
 	ChannelOnError   string          `env:"channel_on_error"`
 	Text             string          `env:"text"`
@@ -53,8 +54,6 @@ type Config struct {
 	Fields          string `env:"fields"`
 	Buttons         string `env:"buttons"`
 }
-
-const messageURL = "https://slack.com/api/chat.postMessage"
 
 // success is true if the build is successful, false otherwise.
 var success = os.Getenv("BITRISE_BUILD_STATUS") == "0"
@@ -110,9 +109,17 @@ func postMessage(conf Config, msg Message) error {
 	}
 	log.Debugf("Request to Slack: %s\n", b)
 
-	req, err := http.NewRequest("POST", messageURL, bytes.NewReader(b))
+	url := string(conf.WebhookURL)
+	if string(conf.APIToken) != "" {
+		url = "https://slack.com/api/chat.postMessage"
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(b))
 	req.Header.Add("Content-Type", "application/json; charset=utf-8")
-	req.Header.Add("Authorization", "Bearer "+string(conf.APIToken))
+
+	if string(conf.APIToken) != "" {
+		req.Header.Add("Authorization", "Bearer "+string(conf.APIToken))
+	}
 
 	client := &http.Client{}
 
