85bd7ba20e176866a0e92c7a435cd25784de6111
Grayson Hansard <grayson.hansard@bitrise.io>
2022-07-13 15:24:30 -0400

    Organize code and add comments

--- main.go
+++ main.go
@@ -12,21 +12,12 @@ import (
 	"os/exec"
 )
 
-func validate(conf *step.Config, logger log.Logger) error {
-	if conf.APIToken == "" && conf.WebhookURL == "" {
-		return fmt.Errorf("Both API Token and WebhookURL are empty. You need to provide one of them. If you want to use incoming webhooks provide the webhook url. If you want to use a bot to send a message provide the bot API token")
-	}
-
-	if conf.APIToken != "" && conf.WebhookURL != "" {
-		logger.Warnf("Both API Token and WebhookURL are provided. Using the API Token")
-		conf.WebhookURL = ""
-
-	}
-	return nil
-}
-
+// Stage A stage represents a discrete unit of work in the execution of this step.  Each stage should do one thing. Due
+// to current imagination limitations, stages can only emit errors but may take references to objects that need to be
+// created or updated during the execution of a stage.
 type Stage func() error
 
+// Execute stages in sequence.  Stop execution if an error is received from a stage.
 func run(stages ...Stage) error {
 	for _, stage := range stages {
 		if err := stage(); err != nil {
@@ -36,6 +27,7 @@ func run(stages ...Stage) error {
 	return nil
 }
 
+// Begin stages
 func parseConfig(conf *step.Config, repo env.Repository) error {
 	if err := stepconf.NewInputParser(repo).Parse(&conf); err != nil {
 		return err
@@ -49,6 +41,19 @@ func enableDebugLog(conf *step.Config, logger log.Logger) error {
 	return nil
 }
 
+func validate(conf *step.Config, logger log.Logger) error {
+	if conf.APIToken == "" && conf.WebhookURL == "" {
+		return fmt.Errorf("Both API Token and WebhookURL are empty. You need to provide one of them. If you want to use incoming webhooks provide the webhook url. If you want to use a bot to send a message provide the bot API token")
+	}
+
+	if conf.APIToken != "" && conf.WebhookURL != "" {
+		logger.Warnf("Both API Token and WebhookURL are provided. Using the API Token")
+		conf.WebhookURL = ""
+
+	}
+	return nil
+}
+
 func createMessage(conf *step.Config, msg *slack.Message) error {
 	*msg = util.NewMessage(*conf)
 	return nil
@@ -58,31 +63,6 @@ func postMessage(api slack.SlackApi, msg *slack.Message, response *slack.SendMes
 	tmp, err := api.Post(msg)
 	response = &tmp
 	return err
-}
-
-func main() {
-	var conf step.Config
-	var msg slack.Message
-	var slackApi slack.SlackApi
-	var response slack.SendMessageResponse
-	envRepo := env.NewRepository()
-	logger := log.NewLogger()
-
-	err := run(
-		func() error { return parseConfig(&conf, envRepo) },
-		func() error { return enableDebugLog(&conf, logger) },
-		func() error { return validate(&conf, logger) },
-		func() error { return createSlackClient(&conf, slackApi, &logger) },
-		func() error { return createMessage(&conf, &msg) },
-		func() error { return postMessage(slackApi, &msg, &response) },
-		func() error { return exportEnvironmentVariables(&response, &conf, logger) },
-	)
-	if err != nil {
-		logger.Errorf("Error: %s", err)
-		os.Exit(1)
-	}
-
-	logger.Donef("\nSlack message successfully sent! 🚀\n")
 }
 
 func exportEnvironmentVariables(response *slack.SendMessageResponse, conf *step.Config, logger log.Logger) error {
@@ -108,4 +88,33 @@ func createSlackClient(conf *step.Config, client slack.SlackApi, logger *log.Log
 		WebhookUrlSelector: &selector,
 	}
 	return nil
+}
+
+// End stages
+
+func main() {
+	envRepo := env.NewRepository()
+	logger := log.NewLogger()
+
+	// Populated by stages
+	var conf step.Config
+	var msg slack.Message
+	var slackApi slack.SlackApi
+	var response slack.SendMessageResponse
+
+	err := run(
+		func() error { return parseConfig(&conf, envRepo) },
+		func() error { return enableDebugLog(&conf, logger) },
+		func() error { return validate(&conf, logger) },
+		func() error { return createSlackClient(&conf, slackApi, &logger) },
+		func() error { return createMessage(&conf, &msg) },
+		func() error { return postMessage(slackApi, &msg, &response) },
+		func() error { return exportEnvironmentVariables(&response, &conf, logger) },
+	)
+	if err != nil {
+		logger.Errorf("Error: %s", err)
+		os.Exit(1)
+	}
+
+	logger.Donef("\nSlack message successfully sent! 🚀\n")
 }
