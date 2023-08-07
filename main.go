6487ee3dab8130fd99ecdef44b5dcecba7c61000
Bálint Mórász <balint.morasz@bitrise.io>
2023-08-07 13:45:16 +0200

    reorder some input fields

--- main.go
+++ main.go
@@ -30,29 +30,29 @@ type Input struct {
 	IconEmojiOnError      string          `env:"emoji_on_error"`
 	IconURL               string          `env:"icon_url"`
 	IconURLOnError        string          `env:"icon_url_on_error"`
+	LinkNames             bool            `env:"link_names,opt[yes,no]"`
 	Username              string          `env:"from_username"`
 	UsernameOnError       string          `env:"from_username_on_error"`
 	ThreadTs              string          `env:"thread_ts"`
 	ThreadTsOnError       string          `env:"thread_ts_on_error"`
 	ReplyBroadcast        bool            `env:"reply_broadcast,opt[yes,no]"`
 	ReplyBroadcastOnError bool            `env:"reply_broadcast_on_error,opt[yes,no]"`
-	LinkNames             bool            `env:"link_names,opt[yes,no]"`
 
 	// Attachment
 	Color           string `env:"color,required"`
 	ColorOnError    string `env:"color_on_error"`
 	PreText         string `env:"pretext"`
 	PreTextOnError  string `env:"pretext_on_error"`
+	AuthorName      string `env:"author_name"`
 	Title           string `env:"title"`
 	TitleOnError    string `env:"title_on_error"`
+	TitleLink       string `env:"title_link"`
 	Message         string `env:"message"`
 	MessageOnError  string `env:"message_on_error"`
 	ImageURL        string `env:"image_url"`
 	ImageURLOnError string `env:"image_url_on_error"`
 	ThumbURL        string `env:"thumb_url"`
 	ThumbURLOnError string `env:"thumb_url_on_error"`
-	AuthorName      string `env:"author_name"`
-	TitleLink       string `env:"title_link"`
 	Footer          string `env:"footer"`
 	FooterIcon      string `env:"footer_icon"`
 	TimeStamp       bool   `env:"timestamp,opt[yes,no]"`
